1d58b5c2d6ff7f458a4a6507f474b3de01fe9bfa
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-04 11:22:07 +0100

    Updated design (computer + iPhone 12)

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ import "./css/style.css"
 const root = document.querySelector("#root")
 
 ReactDOM.render(<App/>, root)
+
+const portfolioBtn = document.querySelector("#portfolio-btn")
+portfolioBtn.addEventListener("click", function() {
+    window.open("https://opixelum.github.io/MyPortfolio")
+})
+
+const linkedinBtn = document.querySelector("#linkedin-btn")
+linkedinBtn.addEventListener("click", function() {
+    window.open("https://linkedin.com/in/anto-benedetti")
+})
+
+const emailBtn = document.querySelector("#email-btn")
+emailBtn.addEventListener("click", function() {
+    window.open("mailto:anto.benedetti@protonmail.com")
+})
