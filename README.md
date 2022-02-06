afc2bf563c7507236614672c1b85e59c94ae7f7f
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-06 14:24:47 +0100

    Added readme, updated paragraphs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Anto's business card
+
+This project was initially for training my React skills but and the end, I can
+use it as a quick overview about me. Moreover, you can find my different
+social medias such as my LinkedIn, Twitter, GitHub but also my portfolio and my
+email.
+
+Since it was a solo project from the FreeCodeCamp React course, I didn't focus
+the styling part with CSS (my goal isn't to be a UX designer). So you may have
+issues with the landscape mode on mobile but anyway, nobody will look at my
+business card in that way.
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,15 @@ export default function Main() {
     return (
         <main>
           <h2>About me</h2>
-          <p>I'm a 18 years old, 1st year CS student at ESGI in Paris. I've
+          <p>I'm 18 years old, a 1st year CS student at ESGI in Paris. I've
           discovered my passion for coding at the age of 8. Aside programming,
           I'm evolved in the NFTs space, but also in the crypto currencies
           trading.</p>
 
           <h2>Interests</h2>
-          <p>What drives me is the blockchain technology. I'm actually focusing
-          on smart contract development for web 3.0 decentralized apps.
-          Moreover, I'm open-minded for working on software/mobile applications
-          projects.</p>
+          <p>What drives me is the blockchain technology. I'm focusing on smart
+          contract development for web 3.0 decentralized apps. Moreover, I'm
+          open-minded for working on software/mobile applications projects.</p>
         </main>
     )
 }
